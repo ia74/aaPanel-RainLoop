3f65223ccd61fedc1f6a7d2046e976eaccd3103c
riiz0r <68617740+riiz0r@users.noreply.github.com>
2020-08-04 20:25:55 -0500

    Delete Main.sh

--- Main.sh
+++
@@ -1,8 +0,0 @@
-while true; do
-    read -p "* [Welcome to the aaPanel-Installers Main Script" yn
-    case $yn in
-        [1]* ) wget;;
-        [2]* ) wget ;;
-        * ) echo "Please select an option!";;
-    esac
-done

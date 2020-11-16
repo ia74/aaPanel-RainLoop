2959f626001258ec4e4e32f5f48e0a3c86a381c9
Roizor <68617740+Roizor@users.noreply.github.com>
2020-11-16 17:53:50 -0600

    Create Experimental-V3.sh

---
+++ Experimental-V3.sh
@@ -0,0 +1,17 @@
+# chmod -R 755 $webVar/$domainVar/webmail
+# chmod -R 777 data
+# chmod -R 777 rainloop
+# chmod -R 666 data/EMPTY
+# chmod -R 644 data/VERSION
+# chmod -R 777 rainloop/*
+# chmod -R 666 index.php
+export webVar=0
+export domainVar=0
+echo "Script by Roizor on GitHub."
+sleep 1
+clear
+echo "Let's begin."
+sleep 1
+clear
+clear
+echo "Downloading necessary files.."

73c39d29ea9e440fd85ec9dce81c0eb95ed80ed3
Roizor <68617740+Roizor@users.noreply.github.com>
2020-12-14 09:14:30 -0600

    yay v3 is a thing now

--- experiments/v3.sh
+++ experiments/v3.sh
@@ -24,7 +24,7 @@ read -p "* [ Enter your TLD. Example: domain.tld ] " domainName
 echo
 read -p "* [ Enter your web root folder. If you don't have one, type /www/wwwroot ] " webFolder
 echo
-echo "* [ TLD: $domainName ]\n * [ Web Root: $webFolder ]"
+echo "* [ TLD: $domainName ]\n* [ Web Root: $webFolder ]"
 echo
 read -p "Does this look right? " yn
 case $yn in
@@ -42,3 +42,38 @@ case $yn in
         * ) echo "Please answer yes or no!";;
     esac
 echo
+rm -rf webmail mail rainloop
+mkdir webmail
+cd webmail
+echo "* [ Fixing permissions ]"
+echo
+chmod -R 755 $webFolder/$domainName/webmail
+chmod -R 777 data
+chmod -R 777 rainloop
+chmod -R 666 data/EMPTY
+chmod -R 644 data/VERSION
+chmod -R 777 rainloop/*
+chmod -R 666 index.php
+echo "* [ Fixed permissions ]"
+cd data
+echo "Deny from all" >> .htaccess
+echo
+echo "* [ Cleaning up ]"
+echo
+rm -rf /tmp/rainloop
+echo "* [ Finished Installing! ]"
+echo
+clear
+sleep 1
+echo "Congratulations!"
+echo
+echo "You have installed RainLoop on $domainName!"
+echo "Login to your installation by going to:"
+echo "$domainName/webmail"
+echo
+echo "Login to the admin panel to configure it."
+echo "$domainName/webmail/?admin"
+echo "Username: admin"
+echo "Password: 12345"
+echo "It is strongly recommended you change this password."
+exit
